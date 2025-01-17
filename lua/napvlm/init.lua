5144a9eb954ea61184d69036000359b91c26f791
napvlm <onlylucknplm@gmail.com>
2025-01-16 21:03:30 -0500

    removed accidental comment

--- lua/napvlm/init.lua
+++ lua/napvlm/init.lua
@@ -4,7 +4,7 @@ vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.cmd("colorscheme onedark")
 vim.cmd("set autoindent expandtab tabstop=2 shiftwidth=2")
--- require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 require("Comment").setup()
 require("guess-indent").setup({})
 require("nvim-treesitter.configs").setup({
